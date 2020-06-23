21239f5e5729710eee7cc1eea02e508e44a7af62
kwamkert <tomriddle477@gmail.com>
2020-06-23 17:04:07 +0000

    featured projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -21,8 +21,10 @@
                     </div>
 
                     <div class="mt-2">
-                        <span class="languages">Java</span>   
+                         <span class="languages">Java</span>  
                             <span class="languages">Javascript</span>
+                            <span class="languages">Firebase</span>
+                            <span class="languages">Heroku</span>
 
                     </div>
                     <div class="mt-3">
@@ -35,6 +37,72 @@
                      
                 </div>
             </div>
+
+
+                <div class="row mt-5">
+            
+                    <div class="col-md-5">
+                        <h4>Admission  System</h4>
+                        <p class="light-text">
+                            This system will help universities manage the status of applications, applicants will also be able to keep track of their applications. 
+                        </p>
+                    
+                        <div >
+                            <span class="languages">Angular</span>   
+                            <span class="languages">Nodejs</span>  
+                            <span class="languages">Express</span>  
+
+                            <span class="languages">Mongo</span>   
+                                <span class="languages">Javascript</span>
+                        </div>
+
+                        <div class="mt-2">
+                                <span class="languages">Netlify</span>
+                                <span class="languages">Heroku</span>
+
+                        </div>
+                        <div class="mt-3">
+                            <a href="https://github.com/KwameKert/admissionportal-client" target="blank"> <img src="@/assets/img/github.svg" class="external-links" alt=""></a>
+                            <a   href="https://admissionportal-client.netlify.app/"  target="blank"> 
+                        <img src="@/assets/img/sign.svg" class="external-links" alt="">
+                            </a>
+                        </div>
+                    
+                        
+                    </div>
+                    <div class="col-md-7">
+                        <img src="@/assets/img/admission.png" class="img-fluid project-image" alt="">
+                    </div>
+            </div>
+
+
+                <div class="row mt-5">
+               
+                <div class="col-md-7">
+                    <img src="@/assets/img/ned.png" class="img-fluid project-image" alt="">
+                </div>
+                <div class="col-md-5">
+                     <h4>Property  System (Proposal)</h4>
+                     <p class="light-text">
+                         This system will help property managers manage property documents efficienty.
+                     </p>
+                   
+                    <div>
+                        <span class="languages">Angular</span>  
+                         <span class="languages">Javascript</span>
+                            <span class="languages">Netlify</span>
+                    </div>
+                    <div class="mt-3">
+                         <a href="https://github.com/KwameKert/nedbank-proposal" target="blank"> <img src="@/assets/img/github.svg" class="external-links" alt=""></a>
+                        <a   href="https://nebank-proposal.web.app/"  target="blank"> 
+                     <img src="@/assets/img/sign.svg" class="external-links" alt="">
+                        </a>
+                    </div>
+                   
+                     
+                </div>
+            </div>
+
         </div>
         <div style="margin-top: 300px"></div>
     </section>
